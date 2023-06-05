2bc85bee8ab652f61d2e0a78e79a2cd5a5c15efe
l3x-fx <kekskiller@email.com>
2023-06-05 20:09:25 +0200

    passed the endpoint test

---
+++ src/tests/utils/resizerSpec.ts
@@ -0,0 +1,12 @@
+import jasmine from "jasmine";
+import express from 'express';
+import sharp from 'sharp';
+import fs from 'fs';
+
+import { resizer } from "../../utils/resizer";
+
+describe('Reszing function test suite', () => {
+    it('tests the resizing', async () => {
+        
+    });
+});
